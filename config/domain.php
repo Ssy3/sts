f6df27b41d75d0be513335b154e6859bd8e067f5
omego <mserihi@gmail.com>
2020-09-17 17:12:44 +0300

    Update domain.php

--- config/domain.php
+++ config/domain.php
@@ -24,6 +24,9 @@ return [
     'staging.comj.live' => 'staging.comj.live',
     'staging.ksauhs.com' => 'staging.ksauhs.com',
     'sts.comj.live' => 'sts.comj.live',
+    'sts.comj.online' => 'sts.comj.online',
+    'sts.conj.online' => 'sts.conj.online',
+    'sts.itjed.com' => 'sts.itjed.com',
     'sts.ksauhs.com' => 'sts.ksauhs.com',
   ],
  ];
